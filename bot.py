4d5abc45a48c763e2d7bbb9a83a9380daeb54275
yngh0m1e <140334733+yngh0m1e@users.noreply.github.com>
2024-04-03 01:44:52 +0500

    bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+import telebot
+from config import token
+
+API_TOKEN = token
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, """\
+                Помогите!!!
+                Нет.\
+""")
+
+@bot.message_handler(commands=['fact'])
+def send_fact(message):
+    bot.reply_to(message, """\
+            Жесть.\
+""")
+
+@bot.message_handler(commands=['joke'])
+def send_joke(message):
+    bot.reply_to(message, """\
+            Иван-дурак приходит к царю:
+— Я выполнил свое обещание, вот голова дракона. А ты выполнишь свое? 
+— Да, вот рука принцессы.\
+""")
+    
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
